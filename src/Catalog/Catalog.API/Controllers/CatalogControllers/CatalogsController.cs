696a15823b0174dd7cdcac24a1b46c16d876c5d5
Maqsudkhan <maqsudtorayev2003@gmail.com>
2024-05-01 01:59:43 +0500

    Create added

---
+++ src/Catalog/Catalog.API/Controllers/CatalogControllers/CatalogsController.cs
@@ -0,0 +1,30 @@
+﻿using Catalog.Application.Abstractions;
+using Catalog.Application.UseCases.CatalogCases.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.API.Controllers.CatalogControllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CatalogsController : ControllerBase
+    {
+        private readonly ICatalogDbContext _context;
+        private readonly IMediator _mediator;
+
+        public CatalogsController(ICatalogDbContext context, IMediator mediator)
+        {
+            _context = context;
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCatalog(CreateCatalogCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+
+    }
+}
